a2c7e6bff02741e2ff37dc54f3b38d3f110dc807
Angelo <macaldo.jonangelo@gmail.com>
2024-01-22 10:05:48 +0800

    error on deployment

--- app/editTopic/[id]/page.tsx
+++ app/editTopic/[id]/page.tsx
@@ -1,5 +1,13 @@
 import EditTopicForm from "@/components/editTopicForm";
 
 export default function EditTopic() {
-    return <div> <EditTopicForm /></div>
+    return <div> {<EditTopicForm task={{
+        todoid: 0,
+        task: "",
+        description: ""
+    }} onCancel={function (): void {
+        throw new Error("Function not implemented.");
+    } } onUpdate={function (updatedTask: { title: string; description: string; }): void {
+        throw new Error("Function not implemented.");
+    } } />}</div>
 }
